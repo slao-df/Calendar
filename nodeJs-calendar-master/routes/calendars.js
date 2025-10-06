02cbc211359ac639209282c67cc05608e07425ab
sooinHan <sooinhan2004@naver.com>
2025-10-07 01:04:11 +0900

    Update calendars.js

--- nodeJs-calendar-master/routes/calendars.js
+++ nodeJs-calendar-master/routes/calendars.js
@@ -1,13 +1,13 @@
-// routes/calendars.js
 const { Router } = require('express');
 const { check } = require('express-validator');
 const { validarCampos } = require('../middlewares/validar-campos');
 const { getCalendars, createCalendar, updateCalendar, deleteCalendar } = require('../controllers/calendars');
 const { validarJWT } = require('../middlewares/validar-jwt');
+const { generarEnlaceCompartir, accederCalendarioCompartido } = require('../controllers/calendarShareController');
 
 const router = Router();
 
-// 이 아래의 모든 라우트는 JWT 검증을 거칩니다.
+// 아래의 모든 라우트는 JWT 검증을 거칩니다.
 router.use(validarJWT);
 
 // 캘린더 목록 가져오기
@@ -28,5 +28,12 @@ router.put('/:id', updateCalendar);
 
 // 캘린더 삭제
 router.delete('/:id', deleteCalendar);
+
+
+// 🔗 공유 링크 생성
+router.post('/:id/share', validarJWT, generarEnlaceCompartir);
+
+// 🔗 초대받은 사용자가 공유 캘린더에 접근
+router.post('/shared/access', validarJWT, accederCalendarioCompartido);
 
 module.exports = router;
