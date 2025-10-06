0515a4f8c68768d5a881c65cea79f71b327b4cbc
sooinHan <sooinhan2004@naver.com>
2025-10-07 01:03:13 +0900

    Update Calendar.js

--- nodeJs-calendar-master/models/Calendar.js
+++ nodeJs-calendar-master/models/Calendar.js
@@ -1,4 +1,3 @@
-// models/Calendar.js
 const { Schema, model } = require('mongoose');
 
 const CalendarSchema = Schema({
@@ -11,12 +10,25 @@ const CalendarSchema = Schema({
     },
     color: {
         type: String,
-        default: '#4A90E2' // 기본 색상
+        default: '#4A90E2'
     },
     user: {
         type: Schema.Types.ObjectId,
-        ref: 'Usuario', // 'Usuario'가 아닌 'User'일 수 있습니다. User 모델의 이름과 일치해야 합니다.
+        ref: 'Usuario',
         required: true
+    },
+    // 🔑 공유 관련 필드
+    shareToken: {
+        type: String,   // 공유 링크에 붙일 토큰 (랜덤 문자열)
+        default: null
+    },
+    sharePassword: {
+        type: String,   // 비밀번호 (bcrypt로 암호화)
+        default: null
+    },
+    isShared: {
+        type: Boolean,
+        default: false
     }
 });
 
