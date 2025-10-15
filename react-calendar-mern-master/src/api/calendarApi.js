e226dfe9b7421e391dbf73f6e6c5ab5ff69fd531
sooinHan <sooinhan2004@naver.com>
2025-10-15 11:47:27 +0900

    Update calendarApi.js

--- react-calendar-mern-master/src/api/calendarApi.js
+++ react-calendar-mern-master/src/api/calendarApi.js
@@ -21,5 +21,17 @@ calendarApi.interceptors.request.use( config => {
     return config;
 })
 
+export const updateCalendar = async (calendarId, calendarData) => {
+    try {
+        // PUT 요청으로 특정 ID의 캘린더 데이터를 업데이트합니다.
+        const { data } = await calendarApi.put(`/calendars/${calendarId}`, calendarData);
+        return data;
+    } catch (error) {
+        console.error('캘린더 업데이트 실패:', error);
+        // 에러를 상위로 다시 던져서 호출한 컴포넌트에서 처리할 수 있게 합니다.
+        throw error;
+    }
+};
+
 
 export default calendarApi;
