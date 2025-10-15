7c9cb4eefe505cbb596f68319dd229056585a539
sooinHan <sooinhan2004@naver.com>
2025-10-15 12:32:08 +0900

    Update AppRouter.jsx

--- react-calendar-mern-master/src/router/AppRouter.jsx
+++ react-calendar-mern-master/src/router/AppRouter.jsx
@@ -6,6 +6,7 @@ import { CalendarPage } from '../calendar';
 import { useAuthStore } from '../hooks';
 import { useEffect } from 'react';
 
+import { PublicCalendarPage } from "../calendar/pages/PublicCalendarPage";
 
 export const AppRouter = () => {
 
@@ -29,22 +30,25 @@ export const AppRouter = () => {
 
   return (
     <Routes>
-            {
-                ( status === 'not-authenticated')  
-                    ? (
-                        <>
-                            <Route path="/auth/*" element={ <LoginPage /> } />
-                            <Route path="/*" element={ <Navigate to="/auth/login" /> } />
-                        </>
-                    )
-                    : (
-                        <>
-                            <Route path="/" element={ <CalendarPage /> } />
-                            <Route path="/*" element={ <Navigate to="/" /> } />
-                        </>
-                    )
-            }
-
-        </Routes>
+        {
+            (status === 'not-authenticated')
+                ? (
+                    <>
+                        <Route path="/auth/*" element={<LoginPage />} />
+                        {/* ❗️ 2. 누구나 접근 가능한 공유 페이지 라우트 추가 */}
+                        <Route path="/share-calendar/:token" element={<PublicCalendarPage />} />
+                        <Route path="/*" element={<Navigate to="/auth/login" />} />
+                    </>
+                )
+                : (
+                    <>
+                        <Route path="/" element={<CalendarPage />} />
+                        {/* ❗️ 3. 로그인한 사용자도 접근 가능하도록 추가 */}
+                        <Route path="/share-calendar/:token" element={<PublicCalendarPage />} />
+                        <Route path="/*" element={<Navigate to="/" />} />
+                    </>
+                )
+        }
+    </Routes>
   )
 }
