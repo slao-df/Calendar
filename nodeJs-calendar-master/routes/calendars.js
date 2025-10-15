bfc9ebbc7b846171109df4a69d0d19b6c26fbdc4
sooinHan <sooinhan2004@naver.com>
2025-10-15 12:32:38 +0900

    Update calendars.js

--- nodeJs-calendar-master/routes/calendars.js
+++ nodeJs-calendar-master/routes/calendars.js
@@ -1,13 +1,20 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
 const { validarCampos } = require('../middlewares/validar-campos');
-const { getCalendars, createCalendar, updateCalendar, deleteCalendar } = require('../controllers/calendars');
+// ❗️ 1. (핵심) 컨트롤러에서 getSharedCalendar 함수를 import 목록에 추가합니다.
+const { getCalendars, createCalendar, updateCalendar, deleteCalendar, getSharedCalendar } = require('../controllers/calendars');
 const { validarJWT } = require('../middlewares/validar-jwt');
 const { generarEnlaceCompartir, accederCalendarioCompartido } = require('../controllers/calendarShareController');
 
 const router = Router();
 
-// 아래의 모든 라우트는 JWT 검증을 거칩니다.
+
+// --- 공개 라우트 (JWT 검증 불필요) ---
+// ❗️ 2. 이 라우트는 로그아웃한 사용자도 접근해야 하므로, JWT 검증 미들웨어 위로 옮깁니다.
+router.get('/share/:token', getSharedCalendar);
+
+
+// --- 인증 필요 라우트 (아래의 모든 라우트는 JWT 검증을 거칩니다.) ---
 router.use(validarJWT);
 
 // 캘린더 목록 가져오기
@@ -31,9 +38,10 @@ router.delete('/:id', deleteCalendar);
 
 
 // 🔗 공유 링크 생성
-router.post('/:id/share', validarJWT, generarEnlaceCompartir);
+router.post('/:id/share', generarEnlaceCompartir); // validarJWT는 router.use로 이미 적용됨
 
 // 🔗 초대받은 사용자가 공유 캘린더에 접근
-router.post('/shared/access', validarJWT, accederCalendarioCompartido);
+router.post('/shared/access', accederCalendarioCompartido); // validarJWT는 router.use로 이미 적용됨
+
 
 module.exports = router;
