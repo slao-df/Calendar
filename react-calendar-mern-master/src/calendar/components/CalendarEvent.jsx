b4d248f296b544426b81488131a1ddd2a484b667
sooinHan <sooinhan2004@naver.com>
2025-10-17 08:08:37 +0900

    Update CalendarEvent.jsx

--- react-calendar-mern-master/src/calendar/components/CalendarEvent.jsx
+++ react-calendar-mern-master/src/calendar/components/CalendarEvent.jsx
@@ -1,11 +1,14 @@
 export const CalendarEvent = ({ event }) => {
 
-    const { title, user } = event;
+    const { title, user, creatorName } = event;
 
     return (
         <>
             <strong>{ title }</strong>
-            <span> - { user.name }</span>
+            {/* 👇 [핵심] creatorName이 있으면 표시하고, 없으면 기존처럼 user.name을 표시합니다.
+                   이렇게 하면 내가 만든 일정은 내 이름이, 공유받은 일정은 원본 작성자 이름이 나옵니다.
+            */}
+            <span> - { creatorName || user.name }</span>
         </>
     )
 }
