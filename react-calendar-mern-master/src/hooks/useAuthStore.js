fd8cc1d590e229e2253af476a454451eaa70a8f1
sooinHan <sooinhan2004@naver.com>
2025-10-17 06:55:58 +0900

    Update useAuthStore.js

--- react-calendar-mern-master/src/hooks/useAuthStore.js
+++ react-calendar-mern-master/src/hooks/useAuthStore.js
@@ -1,36 +1,35 @@
-import { useDispatch, useSelector } from "react-redux"
-import  calendarApi  from "../api/calendarApi";
-import { clearErrorMessage, onChecking, onLogin, onLogout, onLogoutCalendar  } from "../store";
+// src/hooks/useAuthStore.js
 
-
+import { useDispatch, useSelector } from 'react-redux';
+import { calendarApi } from '../api';
+import { clearErrorMessage, onChecking, onLogin, onLogout } from '../store/auth/authSlice';
+// 👇 1. onLogoutCalendar를 import 합니다.
+import { onLogoutCalendar } from '../store/calendar/calendarSlice';
 
 export const useAuthStore = () => {
-
     const { status, user, errorMessage } = useSelector( state => state.auth );
-    const dispatch = useDispatch(); 
-
+    const dispatch = useDispatch();
 
     const startLogin = async({ email, password }) => {
-       
-            dispatch( onChecking() );
-
+        dispatch( onChecking() );
         try {
-            //여기
-            const { data } = await calendarApi.post('/auth', { email, password }); 
-        
+            const { data } = await calendarApi.post('/auth',{ email, password });
             localStorage.setItem('token', data.token );
             localStorage.setItem('token-init-date', new Date().getTime() );
             dispatch( onLogin({ name: data.name, uid: data.uid }) );
             
-        } catch (error) {    
-            
-            
-            dispatch( onLogout('잘못된 로그인 정보') );
+            // 👇 2. [핵심] 로그인 성공 직후, 앱의 '임시 캘린더 목록'을 비웁니다.
+            // 이렇게 하면 CalendarPage가 로딩될 때 서버에서 최신 데이터를 다시 불러옵니다.
+            dispatch( onLogoutCalendar() );
+
+        } catch (error) {
+            dispatch( onLogout('이메일 또는 비밀번호가 올바르지 않습니다.') );
             setTimeout(() => {
                 dispatch( clearErrorMessage() );
             }, 10);
         }
     }
+
     const startRegister = async({ email, password, name }) => {
         dispatch( onChecking() );
         try {
@@ -38,9 +37,11 @@ export const useAuthStore = () => {
             localStorage.setItem('token', data.token );
             localStorage.setItem('token-init-date', new Date().getTime() );
             dispatch( onLogin({ name: data.name, uid: data.uid }) );
+            // 👇 회원가입 직후에도 임시 목록을 비워줍니다.
+            dispatch( onLogoutCalendar() );
             
         } catch (error) {
-            dispatch( onLogout( error.response.data?.msg || '--' ) );
+            dispatch( onLogout(error.response.data?.msg || '--') );
             setTimeout(() => {
                 dispatch( clearErrorMessage() );
             }, 10);
@@ -64,26 +65,21 @@ export const useAuthStore = () => {
 
     const startLogout = () => {
         localStorage.clear();
-        dispatch (onLogoutCalendar());
-        dispatch(onLogout());
+        dispatch( onLogoutCalendar() );
+        dispatch( onLogout() );
     }
 
 
-
     return {
-        //* 속성/특성
-
+        //* Propiedades
         errorMessage,
         status, 
         user, 
 
-
-
-
-        //* 메서드
+        //* Métodos
         checkAuthToken,
         startLogin,
-        startRegister,
         startLogout,
+        startRegister,
     }
 }
