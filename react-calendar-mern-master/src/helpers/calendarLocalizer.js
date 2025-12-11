167cc1ceed3e1974129e99e76ac15d67336ee31c
sooinHan <sooinhan2004@naver.com>
2025-12-12 03:34:21 +0900

    Update calendarLocalizer.js

--- react-calendar-mern-master/src/helpers/calendarLocalizer.js
+++ react-calendar-mern-master/src/helpers/calendarLocalizer.js
@@ -1,24 +1,3 @@
-// // src/helpers/localizer.js
-// import { dateFnsLocalizer } from "react-big-calendar";
-// import { format, parse, startOfWeek, getDay } from "date-fns";
-// import ko from "date-fns/locale/ko";
-
-// const locales = { ko };
-
-// export const localizer = dateFnsLocalizer({
-//   format,
-//   parse,
-//   // react-big-calendar가 (date, culture)를 넘겨주기 때문에 인자를 받아야 함
-//   startOfWeek: (date, culture) =>
-//     startOfWeek(date, {
-//       weekStartsOn: 1,                 // 월요일 시작
-//       locale: locales[culture] || ko,
-//     }),
-//   getDay,
-//   locales,
-// });
-
-
 // helpers/calendarLocalizer.js
 import { dateFnsLocalizer } from 'react-big-calendar';
 import { format, parse, startOfWeek, getDay } from 'date-fns';
@@ -46,7 +25,7 @@ export const localizer = dateFnsLocalizer({
   format: customFormat,
   parse: (value, formatStr) =>
     parse(value, formatStr, new Date(), { locale: ko }),
-  startOfWeek: (date) => startOfWeek(date, { locale: ko }),
+  startOfWeek: (date) => startOfWeek(date, { locale: ko, weekStartsOn: 1 }),
   getDay,
   locales,
 });
